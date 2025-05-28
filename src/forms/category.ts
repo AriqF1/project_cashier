c9dfa545337bfb6b3abbc90f7b4e6534f2b9714a
Ariq Farhan <ariqalthaf445@gmail.com>
2025-05-28 22:12:24 +0700

    Add: Include optional productCount field in category form schema

--- src/forms/category.ts
+++ src/forms/category.ts
@@ -4,6 +4,7 @@ export const categoryFormSchema = z.object({
   name: z.string().min(1, {
     message: "Name is required",
   }),
+  productCount: z.number().optional(),
 });
 
 export type CategoryFormSchema = z.infer<typeof categoryFormSchema>;
