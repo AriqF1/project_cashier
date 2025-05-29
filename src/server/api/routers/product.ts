3f97a46b41fb8ac25e2624d4538e45efe3e265e8
Ariq Farhan <ariqalthaf445@gmail.com>
2025-05-29 20:22:25 +0700

    Add: Implement product router with getProducts query

---
+++ src/server/api/routers/product.ts
@@ -0,0 +1,24 @@
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { z } from "zod";
+
+export const productRouter = createTRPCRouter({
+  getProducts: protectedProcedure.query(async ({ ctx }) => {
+    const { db } = ctx;
+
+    const products = await db.product.findMany({
+      select: {
+        id: true,
+        name: true,
+        price: true,
+        imageUrl: true,
+        category: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+      },
+    });
+    return products;
+  }),
+});
