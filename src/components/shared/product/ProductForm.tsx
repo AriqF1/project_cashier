55d16db383c226e3e78f473d31440cf5803b20ca
Ariq Farhan <ariqalthaf445@gmail.com>
2025-05-29 21:46:13 +0700

    Add: Enhance ProductForm component with category selection using Radix UI Select

--- src/components/shared/product/ProductForm.tsx
+++ src/components/shared/product/ProductForm.tsx
@@ -9,15 +9,41 @@ import type { ProductFormSchema } from "@/forms/products";
 import { useFormContext } from "react-hook-form";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useState } from "react";
+
+type Category = {
+  id: string;
+  name: string;
+};
 
 type ProductFormProps = {
   onSubmit: (values: ProductFormSchema) => void;
+  categories: Category[];
 };
-export const ProductForm = () => {
+
+export const ProductForm = ({ onSubmit, categories }: ProductFormProps) => {
   const form = useFormContext<ProductFormSchema>();
+  const [uploadedImageUrl, setUploadedImageUrl] = useState<string | null>(null);
+
+  const imageChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+
+    if (files && files?.length > 0) {
+      const file = files[0];
+
+      if (!file) return;
+    }
+  };
 
   return (
-    <form action="">
+    <form onSubmit={form.handleSubmit(onSubmit)}>
       <FormField
         control={form.control}
         name="name"
@@ -49,9 +75,24 @@ export const ProductForm = () => {
         name="categoryId"
         render={({ field }) => (
           <FormItem>
-            <FormLabel>Category</FormLabel>
             <FormControl>
-              <Input {...field} />
+              <Select
+                value={field.value}
+                onValueChange={field.onChange}
+                name={field.name}
+                disabled={field.disabled}
+              >
+                <SelectTrigger>
+                  <SelectValue placeholder="Select a category" />
+                </SelectTrigger>
+                <SelectContent>
+                  {categories.map((category) => (
+                    <SelectItem key={category.id} value={category.id}>
+                      {category.name}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
             </FormControl>
             <FormMessage />
           </FormItem>
