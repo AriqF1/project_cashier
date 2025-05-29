cb81f2448dd6c7bc909b7ab987699e819fd19447
Ariq Farhan <ariqalthaf445@gmail.com>
2025-05-29 20:41:44 +0700

    Add: Implement createProduct mutation with validation

--- src/server/api/routers/product.ts
+++ src/server/api/routers/product.ts
@@ -21,4 +21,29 @@ export const productRouter = createTRPCRouter({
     });
     return products;
   }),
+
+  createProduct: protectedProcedure
+    .input(
+      z.object({
+        name: z.string().min(1, "Product name is required"),
+        price: z.coerce.number().min(1000, "Price must be at least 1000"),
+        categoryId: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { db } = ctx;
+
+      const newProduct = await db.product.create({
+        data: {
+          name: input.name,
+          price: input.price,
+          category: {
+            connect: {
+              id: input.categoryId,
+            },
+          },
+        },
+      });
+      return newProduct;
+    }),
 });
